669e0e93c18ffb6b27886649868770411556d3a6
PedroH183 <pedro_freires18@outlook.com>
2025-06-21 13:46:21 -0300

    initial commit

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# app.include_router(routes.router, tags=["VoteManager"], prefix="/api/v1")
+# uvicorn main:app --reload --host 0.0.0.0 --port 8000
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/api/health")
+def root():
+    return {"message": "Working ..."}
