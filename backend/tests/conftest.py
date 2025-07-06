110e4f90298e0995fa416dc3b18aef60ad389912
PedroH183 <pedro_freires18@outlook.com>
2025-07-05 23:31:56 -0300

    fix: update comments for clarity in conftest.py regarding Python path setup for pytest

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 
-# Corrigindo o path para encontrar o executável do Python
-# Isso é necessário para que o pytest encontre os módulos corretamente !!
+# Fixing the Python path for pytest
+# This is necessary for pytest to find the modules correctly
 ROOT_DIR = Path(__file__).resolve().parents[1] # ROOT DIR = backend
 sys.path.append(str(ROOT_DIR))
